a1dce7198a8793ef3429f4b77612085bef22b00a
Ekta Mishra <ektamishra1999@gmail.com>
2021-03-22 18:39:09 +0530

    populate_indices.sh updated

--- test/e2e/populate_indices.sh
+++ test/e2e/populate_indices.sh
@@ -6,5 +6,10 @@ populate_es() {
     curl -XPUT http://localhost:9200/app-000001
 }
 
-
 populate_es
+
+# To insert data 
+NODE_TLS_REJECT_UNAUTHORIZED=0 ./node_modules/.bin/elasticdump \
+    --output=http://localhost:9200 \
+    --input=test-logs-mapping.json \
+    --type=data
