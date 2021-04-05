5688b5b9b6c867c878d0c5f86a595c9552ba9f92
Ekta Mishra <ektamishra1999@gmail.com>
2021-04-05 13:43:50 +0530

    completed controller tests using mockElasticSearchProvider

---
+++ pkg/controllers/logs/controller_test.go
@@ -0,0 +1,245 @@
+package logscontroller
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/ViaQ/log-exploration-api/pkg/elastic"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+)
+
+func Test_ControllerFilterByIndex(t *testing.T) {
+	tests := []struct {
+		TestName   string
+		Index      string
+		ShouldFail bool
+		TestData   []string
+	}{
+		{
+			"Filter by App index",
+			"app",
+			false,
+			[]string{"test-log-1", "test-log-2", "test-log-3"},
+		},
+	}
+
+	provider := elastic.NewMockedElastisearchProvider()
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+	NewLogsController(zap.L(), provider, router)
+
+	for _, tt := range tests {
+		t.Log("Running:", tt.TestName)
+
+		provider.PutDataIntoIndex(tt.Index, tt.TestData)
+
+		rr := httptest.NewRecorder()
+		req, err := http.NewRequest(http.MethodGet, "/logs/indexfilter/app", nil)
+		if err != nil {
+			t.Errorf("Failed to create HTTP request. E: %v", err)
+		}
+		router.ServeHTTP(rr, req)
+		resp := rr.Body.String()
+
+		expected, err := json.Marshal(map[string]interface{}{"Logs": tt.TestData})
+		if err != nil {
+			t.Errorf("failed to marshal test data. E: %v", err)
+		}
+		expectedResp := string(expected)
+
+		if resp != expectedResp {
+			t.Errorf("expected response to be %s, got %s", expectedResp, resp)
+		}
+	}
+
+}
+
+func Test_ControllerGetAllLogs(t *testing.T) {
+	tests := []struct {
+		TestName   string
+		Index      string
+		ShouldFail bool
+		TestData   []string
+	}{
+		{
+			"Get all logs",
+			"infra",
+			false,
+			[]string{"test-log-1", "test-log-2", "test-log-3"},
+		},
+	}
+
+	provider := elastic.NewMockedElastisearchProvider()
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+	NewLogsController(zap.L(), provider, router)
+
+	for _, tt := range tests {
+		t.Log("Running:", tt.TestName)
+
+		provider.PutDataIntoIndex(tt.Index, tt.TestData)
+
+		rr := httptest.NewRecorder()
+		req, err := http.NewRequest(http.MethodGet, "/logs/", nil)
+		if err != nil {
+			t.Errorf("Failed to create HTTP request. E: %v", err)
+		}
+		router.ServeHTTP(rr, req)
+		resp := rr.Body.String()
+
+		expected, err := json.Marshal(map[string]interface{}{"Logs": tt.TestData})
+		if err != nil {
+			t.Errorf("failed to marshal test data. E: %v", err)
+		}
+		expectedResp := string(expected)
+
+		if resp != expectedResp {
+			t.Errorf("expected response to be %s, got %s", expectedResp, resp)
+		}
+	}
+
+}
+
+
+func Test_ControllerFilterByTime(t *testing.T) {
+	tests := []struct {
+		TestName   string
+		Index      string
+		ShouldFail bool
+		TestData   []string
+	}{
+		{
+			"Filter by time",
+			"infra",
+			false,
+			[]string{"test-log-1", "test-log-2", "test-log-3"},
+		},
+	}
+
+	provider := elastic.NewMockedElastisearchProvider()
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+	NewLogsController(zap.L(), provider, router)
+
+	for _, tt := range tests {
+		t.Log("Running:", tt.TestName)
+
+		logTime,_ := time.Parse(time.RFC3339Nano, "2021-03-17T14:22:40+05:30")
+		provider.PutDataAtTime(logTime, tt.TestData)
+
+		rr := httptest.NewRecorder()
+		req, err := http.NewRequest(http.MethodGet, "/logs/timefilter/2021-03-17T14:22:20+05:30/2021-03-17T14:23:20+05:30", nil)
+		if err != nil {
+			t.Errorf("Failed to create HTTP request. E: %v", err)
+		}
+		router.ServeHTTP(rr, req)
+		resp := rr.Body.String()
+
+		expected, err := json.Marshal(map[string]interface{}{"Logs": tt.TestData})
+		if err != nil {
+			t.Errorf("failed to marshal test data. E: %v", err)
+		}
+		expectedResp := string(expected)
+
+		if resp != expectedResp {
+			t.Errorf("expected response to be %s, got %s", expectedResp, resp)
+		}
+	}
+
+}
+
+func Test_FilterLogsByPodName(t *testing.T) {
+	tests := []struct {
+		TestName   string
+		Index      string
+		ShouldFail bool
+		TestData   []string
+	}{
+		{
+			"Filter by Podname",
+			"infra",
+			false,
+			[]string{"test-log-1 test-log-1 pod_name: openshift-kube-scheduler-ip-10-0-157-165.ec2.internal"},
+		},
+	}
+
+	provider := elastic.NewMockedElastisearchProvider()
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+	NewLogsController(zap.L(), provider, router)
+
+	for _, tt := range tests {
+		t.Log("Running:", tt.TestName)
+
+		provider.PutDataIntoIndex(tt.Index, tt.TestData)
+
+		rr := httptest.NewRecorder()
+		req, err := http.NewRequest(http.MethodGet, "/logs/podnamefilter/openshift-kube-scheduler-ip-10-0-157-165.ec2.internal", nil)
+		if err != nil {
+			t.Errorf("Failed to create HTTP request. E: %v", err)
+		}
+		router.ServeHTTP(rr, req)
+		resp := rr.Body.String()
+
+		expected, err := json.Marshal(map[string]interface{}{"Logs": tt.TestData})
+		if err != nil {
+			t.Errorf("failed to marshal test data. E: %v", err)
+		}
+		expectedResp := string(expected)
+
+		if resp != expectedResp {
+			t.Errorf("expected response to be %s, got %s", expectedResp, resp)
+		}
+	}
+
+}
+
+func Test_FilterLogsMultipleParameters(t *testing.T) {
+	tests := []struct {
+		TestName   string
+		Index      string
+		ShouldFail bool
+		TestData   []string
+	}{
+		{
+			"Filter by multiple parameters",
+			"infra",
+			false,
+			[]string{"test-log-1 pod_name: openshift-kube-scheduler-ip-10-0-157-165.ec2.internal, namespace_name: openshift-kube-scheduler"},
+		},
+	}
+
+	provider := elastic.NewMockedElastisearchProvider()
+	gin.SetMode(gin.TestMode)
+	router := gin.Default()
+	NewLogsController(zap.L(), provider, router)
+
+	for _, tt := range tests {
+		t.Log("Running:", tt.TestName)
+		logTime,_ := time.Parse(time.RFC3339Nano, "2021-03-17T14:22:40+05:30")
+		provider.PutDataAtTime(logTime, tt.TestData)
+
+		rr := httptest.NewRecorder()
+		req, err := http.NewRequest(http.MethodGet, "/logs/multifilter/openshift-kube-scheduler/openshift-kube-scheduler-ip-10-0-157-165.ec2.internal/2021-03-17T14:22:20+05:30/2021-03-17T14:23:20+05:30", nil)
+		if err != nil {
+			t.Errorf("Failed to create HTTP request. E: %v", err)
+		}
+		router.ServeHTTP(rr, req)
+		resp := rr.Body.String()
+
+		expected, err := json.Marshal(map[string]interface{}{"Logs": tt.TestData})
+		if err != nil {
+			t.Errorf("failed to marshal test data. E: %v", err)
+		}
+		expectedResp := string(expected)
+
+		if resp != expectedResp {
+			t.Errorf("expected response to be %s, got %s", expectedResp, resp)
+		}
+	}
+
+}
---
+++ pkg/elastic/mock.go
@@ -0,0 +1,134 @@
+package elastic
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/ViaQ/log-exploration-api/pkg/logs"
+)
+
+type MockedElasticsearchProvider struct {
+	App   map[time.Time][]string
+	Infra map[time.Time][]string
+	Audit map[time.Time][]string
+}
+
+func NewMockedElastisearchProvider() *MockedElasticsearchProvider {
+	return &MockedElasticsearchProvider{
+		App:   map[time.Time][]string{},
+		Infra: map[time.Time][]string{},
+		Audit: map[time.Time][]string{},
+	}
+}
+
+func (m *MockedElasticsearchProvider) PutDataIntoIndex(index string, data []string) error {
+	switch strings.ToLower(index) {
+	case "app":
+		m.App[time.Now()] = data
+		return nil
+	case "infra":
+		m.Infra[time.Now()] = data
+		return nil
+	case "audit":
+		m.Infra[time.Now()] = data
+		return nil
+	default:
+		return errors.New("unknown index")
+	}
+}
+
+func (m *MockedElasticsearchProvider) PutDataAtTime(logTime time.Time, data []string) error {
+	m.Infra[logTime] = data
+	return nil
+}
+
+func (m *MockedElasticsearchProvider) FilterByIndex(index string) ([]string, error) {
+	var lg map[time.Time][]string
+	switch strings.ToLower(index) {
+	case "app":
+		lg = m.App
+	case "infra":
+		lg = m.Infra
+	case "audit":
+		lg = m.Audit
+	}
+	if len(lg) == 0 {
+		return nil, logs.NotFoundError()
+	}
+
+	result := []string{}
+	for _, v := range lg {
+		result = append(result, v...)
+	}
+	return result, nil
+}
+
+func (m *MockedElasticsearchProvider) FilterByTime(startTime time.Time, finishTime time.Time) ([]string, error) {
+	var lg map[time.Time][]string
+	lg = m.Infra
+	if len(lg) == 0 {
+		return nil, logs.NotFoundError()
+	}
+
+	result := []string{}
+	for k, v := range lg {
+		if k.After(startTime) && k.Before(finishTime) {
+			result = append(result, v...)
+		}
+	}
+	return result, nil
+}
+
+func (m *MockedElasticsearchProvider) GetAllLogs() ([]string, error) {
+	var lg map[time.Time][]string
+	lg = m.Infra
+	
+	if len(lg) == 0 {
+		return nil, logs.NotFoundError()
+	}
+
+	result := []string{}
+	for _, v := range lg {
+		result = append(result, v...)
+	}
+	return result, nil
+}
+
+func (m *MockedElasticsearchProvider) FilterByPodName(podName string) ([]string, error) {
+	var lg map[time.Time][]string
+	lg = m.Infra
+	
+	if len(lg) == 0 {
+		return nil, logs.NotFoundError()
+	}
+
+	result := []string{}
+	for _, v := range lg {
+		if strings.Contains(v[0], "pod_name: "+podName) {
+			result = append(result, v...)
+		}
+	}
+	return result, nil
+}
+
+func (m *MockedElasticsearchProvider) FilterLogsMultipleParameters(podName string, namespace string, startTime time.Time, finishTime time.Time) ([]string, error) {
+	var lg map[time.Time][]string
+	lg = m.Infra
+	
+	if len(lg) == 0 {
+		return nil, logs.NotFoundError()
+	}
+
+	result := []string{}
+	for k, v := range lg {
+		fmt.Print(v[0])
+		if k.After(startTime) && k.Before(finishTime) &&
+		 strings.Contains(v[0], "pod_name: ") &&
+		 strings.Contains(v[0], "namespace_name: ") {
+			result = append(result, v...)
+		}
+	}
+	return result, nil
+}
