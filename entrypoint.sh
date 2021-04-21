4cee295bd49d0f71c4615a665944c387f9d2ebee
Akshay Bhansali <abhansal@abhansal.remote.csb>
2021-04-21 19:48:35 +0530

    add ES_TLS as environment variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,14 @@ set -eou pipefail
 : ${ES_ADDR:="https://localhost:9200"}
 : ${ES_CERT:="admin-cert"}
 : ${ES_KEY:="admin-key"}
+: ${ES_TLS:= "false"}
 
 if [ "$1" = "log-exploration-api" ]; then
 	exec log-exploration-api \
 		-es-addr=${ES_ADDR} \
 		-es-cert=${ES_CERT} \
-		-es-key=${ES_KEY}
+		-es-key=${ES_KEY} \
+		-es-tls=${ES_TLS}
 fi
 
 exec "$@"
