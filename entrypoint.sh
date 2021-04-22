c2ac26fe3b7be0893617fb04fa89b59f9c5b2c0d
Ekta Mishra <ektamishra1999@gmail.com>
2021-04-22 21:12:08 +0530

    Updated ES_TLS env variable value with boolean

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -eou pipefail
 : ${ES_ADDR:="https://localhost:9200"}
 : ${ES_CERT:="admin-cert"}
 : ${ES_KEY:="admin-key"}
-: ${ES_TLS:= "false"}
+: ${ES_TLS:= false}
 
 if [ "$1" = "log-exploration-api" ]; then
 	exec log-exploration-api \
