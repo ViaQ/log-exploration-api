e99e725ce31ddc05f88a3e9b129c2c0ed7f56046
Yashwanthreddy Beeravolu <ybeeravo@ybeeravo.remote.csb>
2021-07-14 08:44:08 -0400

    refactoring the usage of header in the health controller

--- pkg/controllers/health/controller.go
+++ pkg/controllers/health/controller.go
@@ -15,18 +15,17 @@ func NewHealthController(router *gin.Engine, logsProvider logs.LogsProvider) {
 	healthController := &HealthController{
 		healthProvider: logsProvider,
 	}
+	router.Use(middleware.AddHeader())
 	r := router.Group("")
 	r.GET("/health", healthController.HealthHandler)
 	r.GET("/ready", healthController.ReadinessHandler)
 }
 
 func (healthController *HealthController) HealthHandler(gctx *gin.Context) {
-	middleware.AddHeader()
 	gctx.JSON(http.StatusOK, gin.H{"Message": "Success"})
 }
 
 func (healthController *HealthController) ReadinessHandler(gctx *gin.Context) {
-	middleware.AddHeader()
 	checkReadiness := healthController.healthProvider.CheckReadiness()
 	if checkReadiness == false {
 		gctx.JSON(http.StatusBadRequest, gin.H{"Message": "failed to connect to esClient"})
